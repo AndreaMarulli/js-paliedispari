41aff5e943418f149ee930cb6d0bc9848365cdb0
AndreaMarulli <amandreamarulli@gmail.com>
2025-02-28 14:50:43 +0100

    feat: corretto il primo esercizio

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma (provate a implementare questa funzione con il ciclo for)
-const utente = prompt('inserisci una parola')
-console.log(utente);
 
-const word = "Anna";
-for (let i = 0; i < word.length; i++) {
-    let truFalse;
-    if (word % 2 === 0) {
-        truFalse = "true";
-    } else {
-        truFalse = "false";
-    }
-    console.log(i, truFalse);
+const utente = "ciao";
+
+const palindroma = worldPalindorma(utente);
+console.log(palindroma);
+
+
+function worldPalindorma (parola) { 
+  let reverseWord = "";
+  for (let i = parola.length - 1; i >= 0; i--) {
+    const Letter = parola.charAt(i);
+    reverseWord += Letter;
+  }
+
+  console.log(reverseWord === parola);
+  
 }
-
 
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 6.
@@ -22,3 +25,4 @@ for (let i = 0; i < word.length; i++) {
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
+
